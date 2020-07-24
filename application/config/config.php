636837451e4ff5351771afcf0a9d4a937342a8fa
haxan100 <abdul.hasan388@gmail.com>
2020-07-24 22:52:52 +0700

    membuat site dinamis

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gantela/';
+// $config['base_url'] = 'http://localhost/gantela/';
+$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
+$url = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "$http" . $_SERVER['SERVER_NAME'] . "" . $url;
+
 
 /*
 |--------------------------------------------------------------------------
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,6 +1,7 @@
 					<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 						<div class="menu_section">
 							<h3>General</h3>
+							<!-- <?php echo base_url(); ?> -->
 							<ul class="nav side-menu">
 								<li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
 									<ul class="nav child_menu">
