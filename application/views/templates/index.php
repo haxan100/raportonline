cba227638f52f131e6715e4ddfe46d3a8fc0c0d0
haxan100 <abdul.hasan388@gmail.com>
2020-08-20 23:02:22 +0700

    perbaikan sidebar

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -6,7 +6,7 @@
 			<div class="col-md-3 left_col">
 				<div class="left_col scroll-view">
 					<div class="navbar nav_title" style="border: 0;">
-						<a href="index.html" class="site_title">Raport Online<i class="fa fa-paw"></i> <span> </span></a>
+						<a href="dashboard" class="site_title">Raport Online<i class="fa fa-paw"></i> <span> </span></a>
 					</div>
 
 					<div class="clearfix"></div>
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,101 +6,36 @@
 								<li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
 									<ul class="nav child_menu">
 										<li><a href="<?php echo site_url('dashboard') ?>">Dashbsoard</a></li>
-										<li><a href="<?php echo site_url('siswa') ?>">Siswa</a></li>
-										<li><a href="<?php echo site_url('siswa/Kelas') ?>">Kelas</a></li>
-										<li><a href="<?php echo site_url('siswa/Mapel') ?>">Mapel</a></li>
 										<li><a href="<?php echo site_url('Wali') ?>">Wali Kelas</a></li>
-										<li><a href="<?php echo site_url('Wali/Guru') ?>">Guru</a></li>
-										<li><a href="<?php echo site_url('Nilai') ?>">Isi Nilai</a></li>
-										<li><a href="<?php echo site_url('Konfig') ?>">Konfig</a></li>
 									</ul>
 								</li>
-								<li><a><i class="fa fa-edit"></i> Cetak <span class="fa fa-chevron-down"></span></a>
+								<li><a><i class="glyphicon glyphicon-user"></i> Master  <span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="<?php echo site_url('siswa') ?>">Master Siswa</a></li>
+										<li><a href="<?php echo site_url('siswa/Kelas') ?>">Master Kelas</a></li>
+										<li><a href="<?php echo site_url('siswa/Mapel') ?>">Master Mapel</a></li>										
+										<li><a href="<?php echo site_url('Wali/Guru') ?>">Master Guru</a></li>
+									</ul>
+								</li>
+								<li><a><i class="fa fa-edit"></i> Nilai  <span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="<?php echo site_url('Nilai') ?>">Isi Nilai</a></li>
+									</ul>
+								</li>
+								<li><a><i class="glyphicon glyphicon-print"></i> Cetak <span class="fa fa-chevron-down"></span></a>
 									<ul class="nav child_menu">
 										<li><a href="<?php echo site_url('Laporan/SiswaAll') ?>">Semua Siswa</a></li>
 										<li><a href="<?php echo site_url('Laporan/GuruAll') ?>">Semua Guru</a></li>
 										<li><a href="<?php echo site_url('Laporan/MapelAll') ?>">Semua Mapel</a></li>
-										<li><a href="form_wizards.html">Form Wizard</a></li>
-										<li><a href="form_upload.html">Form Upload</a></li>
-										<li><a href="form_buttons.html">Form Buttons</a></li>
 									</ul>
 								</li>
-								<li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
+								<li><a><i class="fa fa-desktop"></i> Administrator<span class="fa fa-chevron-down"></span></a>
 									<ul class="nav child_menu">
-										<li><a href="general_elements.html">General Elements</a></li>
-										<li><a href="media_gallery.html">Media Gallery</a></li>
-										<li><a href="typography.html">Typography</a></li>
-										<li><a href="icons.html">Icons</a></li>
-										<li><a href="glyphicons.html">Glyphicons</a></li>
-										<li><a href="widgets.html">Widgets</a></li>
-										<li><a href="invoice.html">Invoice</a></li>
-										<li><a href="inbox.html">Inbox</a></li>
-										<li><a href="calendar.html">Calendar</a></li>
+									
+										<li><a href="<?php echo site_url('Konfig') ?>">Konfig</a></li>
+									
 									</ul>
-								</li>
-								<li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-									<ul class="nav child_menu">
-										<li><a href="tables.html">Tables</a></li>
-										<li><a href="tables_dynamic.html">Table Dynamic</a></li>
-									</ul>
-								</li>
-								<li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-									<ul class="nav child_menu">
-										<li><a href="chartjs.html">Chart JS</a></li>
-										<li><a href="chartjs2.html">Chart JS2</a></li>
-										<li><a href="morisjs.html">Moris JS</a></li>
-										<li><a href="echarts.html">ECharts</a></li>
-										<li><a href="other_charts.html">Other Charts</a></li>
-									</ul>
-								</li>
-								<li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-									<ul class="nav child_menu">
-										<li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-										<li><a href="fixed_footer.html">Fixed Footer</a></li>
-									</ul>
-								</li>
-							</ul>
-						</div>
-						<div class="menu_section">
-							<h3>Live On</h3>
-							<ul class="nav side-menu">
-								<li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
-									<ul class="nav child_menu">
-										<li><a href="e_commerce.html">E-commerce</a></li>
-										<li><a href="projects.html">Projects</a></li>
-										<li><a href="project_detail.html">Project Detail</a></li>
-										<li><a href="contacts.html">Contacts</a></li>
-										<li><a href="profile.html">Profile</a></li>
-									</ul>
-								</li>
-								<li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-									<ul class="nav child_menu">
-										<li><a href="page_403.html">403 Error</a></li>
-										<li><a href="page_404.html">404 Error</a></li>
-										<li><a href="page_500.html">500 Error</a></li>
-										<li><a href="plain_page.html">Plain Page</a></li>
-										<li><a href="login.html">Login Page</a></li>
-										<li><a href="pricing_tables.html">Pricing Tables</a></li>
-									</ul>
-								</li>
-								<li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
-									<ul class="nav child_menu">
-										<li><a href="#level1_1">Level One</a>
-										<li><a>Level One<span class="fa fa-chevron-down"></span></a>
-											<ul class="nav child_menu">
-												<li class="sub_menu"><a href="level2.html">Level Two</a>
-												</li>
-												<li><a href="#level2_1">Level Two</a>
-												</li>
-												<li><a href="#level2_2">Level Two</a>
-												</li>
-											</ul>
-										</li>
-										<li><a href="#level1_2">Level One</a>
-										</li>
-									</ul>
-								</li>
-								<li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
+		
 							</ul>
 						</div>
 
